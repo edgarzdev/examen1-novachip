6f9e8e8b4db14ee0c8c5d164f287360cac44f14c
Edgar Ramirez <01edgar.rz@gmail.com>
2025-08-04 03:38:52 -0400

    Agregar MarcaController con métodos CRUD básicos

--- novachip/app/Http/Controllers/MarcaController.php
+++ novachip/app/Http/Controllers/MarcaController.php
@@ -2,9 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Marca;
 use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
-    //
+    // Mostrar todas las marcas
+    public function index()
+    {
+        return response()->json(Marca::all(), 200);
+    }
+    // Crear nueva marca
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required|string|max:255',
+        ]);
+
+        $marca = Marca::create($request->all());
+
+        return response()->json([
+            'mensaje' => 'marca creada correctamente',
+            'marca' => $marca
+        ], 201);
+    }
+    public function show(string $id)
+    {
+        $marca = Marca::find($id);
+        if (!$marca) {
+            return response()->json([
+                'mensaje' => 'marca no encontrada'
+            ], 404);
+        }
+
+        return response()->json([
+            'marca' => $marca
+        ], 200);
+    }
+    // Actualizar una marca
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'nombre' => 'required|string|max:255',
+        ]);
+        $marca = Marca::find($id);
+        if (!$marca) {
+            return response()->json([
+                'mensaje' => 'marca no encontrada'
+            ], 404);
+        }
+        $marca->update($request->all());
+
+        return response()->json(
+            [
+                'mensaje'=>'Marca actualizada correctamente',
+                'marca' =>$marca
+            ],
+            200
+        );
+    }
+
+    // Eliminar una marca
+    public function destroy(Marca $marca)
+    {
+        $marca->delete();
+
+        return response()->json(['mensaje' => 'Marca eliminada'], 200);
+    }
 }
